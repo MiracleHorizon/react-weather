cb430fb206d290d3f3b8881efc03f09b3791c815
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-05-26 18:35:54 +0300

    refactor: собственный тип ChildrenProps удален и заменен на встроенный тип от React - PropsWithChildren
    feat: изменены цвета фона приложения

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import { Rubik } from 'next/font/google'
 import type { Metadata } from 'next'
+import type { PropsWithChildren } from 'react'
 
 import { useSelectTheme } from '@hooks/useSelectTheme'
-import type { ChildrenProps } from '@app-types/ChildrenProps'
 import '@public/styles/globals.css'
 
 const rubik = Rubik({
@@ -21,13 +21,13 @@ export const metadata: Metadata = {
   }
 }
 
-export default function RootLayout({ children }: ChildrenProps) {
+export default function RootLayout({ children }: PropsWithChildren) {
   const { withDarkMode } = useSelectTheme()
 
   return (
     <html lang='en' className={withDarkMode ? 'dark' : 'light'}>
       <body className={`${rubik.className} relative`}>
-        <div className='relative flex h-screen w-screen items-center justify-center bg-[#fdfdff] px-[24px] dark:bg-gray-700 [440px-max]:px-[16px] [650px-max]:items-start [650px-max]:pt-[24px]'>
+        <div className='relative flex h-screen w-screen items-center justify-center bg-gradient-to-r from-amber-300 to-orange-500 px-[24px] dark:bg-gradient-to-br dark:from-slate-600 dark:to-gray-800 [440px-max]:px-[16px] [650px-max]:items-start [650px-max]:pt-[24px]'>
           {children}
         </div>
       </body>
