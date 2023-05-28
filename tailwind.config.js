19c880800b2e31130b4fb4932b0821b4f35cd938
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-05-28 21:14:49 +0300

    refactor: проведен рефакторинг tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,51 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+const { screens } = require('tailwindcss/defaultTheme')
 
 const tailwindConfig = {
   darkMode: 'class',
   content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      gridTemplateColumns: {
-        'rep-2': 'repeat(2, 1fr)'
-      },
-      gridTemplateRows: {
-        'rep-2': 'repeat(2, 1fr)'
-      }
-    },
     screens: {
-      '[650px-max]': {
-        max: '650px'
-      },
-      '[550px-max]': {
-        max: '550px'
+      '[400px-max]': {
+        max: '400px'
       },
       '[440px-max]': {
         max: '440px'
       },
-      '[440px-min]': {
-        min: '440px'
+      '[550px-max]': {
+        max: '550px'
       },
-      '[400px-max]': {
-        max: '400px'
+      '[650px-max]': {
+        max: '650px'
       },
-      'sm-max': {
-        max: '576px'
-      },
-      'md-max': {
-        max: '768px'
-      },
-      'lg-max': {
-        max: '992px'
-      },
-      'xl-max': {
-        max: '1200px'
-      },
-      'xxl-max': {
-        max: '1400px'
-      }
+      ...screens
     }
-  },
-  plugins: []
+  }
 }
 
 module.exports = tailwindConfig
