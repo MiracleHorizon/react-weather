48d0c441c4a20ac1ba38ae41fec714992d634729
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-06-06 02:51:33 +0300

    06.06.2023, 02:40
    refactor: проведен рефакторинг стилей компонента WeeklyForecast
    
    feat: добавлены стили для темной темы в компоненте WeeklyForecast

--- app/forecast/components/ForecastContent/WeeklyForecast/WeeklyForecast.tsx
+++ app/forecast/components/ForecastContent/WeeklyForecast/WeeklyForecast.tsx
@@ -1,4 +1,5 @@
 import dynamic from 'next/dynamic'
+import { twJoin } from 'tailwind-merge'
 
 import WeeklyForecastListSkeleton from './WeeklyForecastListSkeleton'
 
@@ -10,7 +11,17 @@ const WeeklyForecastList = dynamic(() => import('./WeeklyForecastList'), {
 export default function WeeklyForecast() {
   return (
     <footer className='mt-[24px] w-full [440px-max]:mt-0'>
-      <h3 className='mb-[6px] hidden w-full text-center text-[20px] [440px-max]:inline-block'>
+      <h3
+        className={twJoin([
+          'mb-[6px]',
+          'hidden',
+          'w-full',
+          'text-center',
+          'text-[20px]',
+          'dark:text-gray-200',
+          '[440px-max]:inline-block'
+        ])}
+      >
         Week
       </h3>
       <WeeklyForecastList />
