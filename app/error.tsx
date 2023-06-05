8a6cafd4008919a831bb145c9e6ded7d78720718
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-06-05 03:36:42 +0300

    05.06.2023, 03:30
    feat: добавлена обработка WrongCredentialsException в error.tsx

--- app/error.tsx
+++ app/error.tsx
@@ -1,7 +1,13 @@
 'use client'
 
+import WrongCredentials from '@components/WrongCredentials'
+import { WrongCredentialsException } from '@exceptions/WrongCredentialsException'
 import type { ErrorPageProps } from '@app-types/ErrorPageProps'
 
 export default function ErrorPage({ error }: ErrorPageProps) {
-  console.log(error)
+  if (error.message === WrongCredentialsException.getMessage()) {
+    return <WrongCredentials />
+  }
+
+  return <h1>Error</h1>
 }
